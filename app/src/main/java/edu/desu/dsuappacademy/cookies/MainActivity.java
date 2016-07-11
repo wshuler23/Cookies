85719d4fe9806fec5e6a89c29b2e001707600eb9
dsuappacademy <wshuler23@gmail.com>
2016-07-11 16:23:28 -0400

    Added other files

--- app/src/main/java/edu/desu/dsuappacademy/cookies/MainActivity.java
+++ app/src/main/java/edu/desu/dsuappacademy/cookies/MainActivity.java
@@ -1,8 +1,12 @@
 package edu.desu.dsuappacademy.cookies;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +22,26 @@ public class MainActivity extends AppCompatActivity {
     public void eatCookie(View view) {
         // TODO: Find a reference to the ImageView in the layout. Change the image.
 
+        ImageView after = (ImageView) findViewById(R.id.android_cookie_image_view);
+        after.setImageResource(R.drawable.after);
+
 
         // TODO: Find a reference to the TextView in the layout. Change the text.
 
+        TextView full = (TextView) findViewById(R.id.status_text_view);
+        full.setText("I'm so full");
+    }
+
+    public void reset(View view) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+
+        ImageView after = (ImageView) findViewById(R.id.android_cookie_image_view);
+        after.setImageResource(R.drawable.before);
+
+
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+
+        TextView full = (TextView) findViewById(R.id.status_text_view);
+        full.setText("I'm so hungry");
     }
 }
